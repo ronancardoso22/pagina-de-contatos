1791d62c68b6b54a1418201f046f849132ae5f7a
ronan cardoso <ronancardoso22@hotmail.com>
2023-02-14 14:52:31 -0400

    projeto concluido desktop

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-function toggleMode ()
+function toggleMode () {
+
+    const html = document.documentElement
+
+
+    html.classList.toggle('light')
+ //   if(html.classList.contains('light')) {
+ //       html.classList.remove('light')
+ //   } else{
+ //       html.classList.add('light')
+ //   }
+
+       
+
+        // pegar a tag img 
+
+        const img = document.querySelector("#profile img")
+       
+        // substituir a imagem do perfil
+
+        if(html.classList.contains('light')) {
+             // se tiver light mode adicionar perfil light
+                img.setAttribute('src','./assets/profileLight.png')
+
+         } else {
+
+             img.setAttribute('src','./assets/profile.png')
+         }
+}
