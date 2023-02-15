d1cb43c681d4d4d57eb3b7f48cdc38f6e742495b
ronan cardoso <ronancardoso22@hotmail.com>
2023-02-15 12:08:14 -0400

    concertado para mobile com slide de botao

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function toggleMode () {
     const html = document.documentElement
 
 
-    html.classList.toggle('light')
+   html.classList.toggle('light')
  //   if(html.classList.contains('light')) {
- //       html.classList.remove('light')
+ //     html.classList.remove('light')
  //   } else{
  //       html.classList.add('light')
  //   }
